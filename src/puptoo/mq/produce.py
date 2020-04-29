f44f5d74a606b765311e119fd8f185fcc59f0083
Vinay Kapalavai <vkapalav@redhat.com>
2020-04-29 17:15:54 -0400

    Add message.max.bytes parameter in the producer config

--- src/puptoo/mq/produce.py
+++ src/puptoo/mq/produce.py
@@ -4,5 +4,5 @@ from ..utils import config
 
 
 def init_producer():
-    producer = Producer({"bootstrap.servers": ",".join(config.BOOTSTRAP_SERVERS)})
+    producer = Producer({"bootstrap.servers": ",".join(config.BOOTSTRAP_SERVERS), "message.max.bytes": int("20000000")})
     return producer
